eb59b97c73cb37c0eee92d386d2c358701e9b661
insulyna <166237016+insulyna@users.noreply.github.com>
2024-04-06 11:10:43 -0500

    Update script.js
    1

--- quieressermisanvalentin-main/quieressermisanvalentin-main/script.js
+++ quieressermisanvalentin-main/quieressermisanvalentin-main/script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = '¡Oh Siii! jajaja';
+    document.getElementById('messageContainer').innerHTML = '¡Osisisis! uwuwu';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
